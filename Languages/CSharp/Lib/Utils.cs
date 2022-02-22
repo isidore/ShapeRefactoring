216d8049d02fdc0c0bb37cd322c06d46cb794a4f
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-02-22 14:41:37 +0000

    r   Delete curly braces

--- Languages/CSharp/Lib/Utils.cs
+++ Languages/CSharp/Lib/Utils.cs
@@ -72,10 +72,8 @@ namespace Shape.Lib
             dynamic tack;
 
             dynamic gs;
-            {
-                tack = (dynamic)MathHelper.Gs(0);
-                gs = MathHelper.Gz();
-            }
+            tack = (dynamic) MathHelper.Gs(0);
+            gs = MathHelper.Gz();
             gs = gs(1);
 
             Applesauce(roster, gs, tack);
